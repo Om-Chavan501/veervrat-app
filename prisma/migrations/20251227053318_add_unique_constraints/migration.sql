4bc41576e390c29c71c3f432c33eec4d3dd3e6bc
Om-Chavan501 <om.chavan501@gmail.com>
2025-12-27 11:34:16 +0530

    ontology details seeded

---
+++ prisma/migrations/20251227053318_add_unique_constraints/migration.sql
@@ -0,0 +1,11 @@
+-- DropIndex
+DROP INDEX "Lacuna_nameMr_key";
+
+-- DropIndex
+DROP INDEX "Sentence_textMr_key";
+
+-- DropIndex
+DROP INDEX "SubVirtue_nameMr_key";
+
+-- DropIndex
+DROP INDEX "Virtue_nameMr_key";
