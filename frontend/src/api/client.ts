68ff299ce5088bdfa46cc88f20e474bbe73edc3a
Om-Chavan501 <om.chavan501@gmail.com>
2026-03-15 17:03:43 +0530

    env loading for frontend and cors

--- frontend/src/api/client.ts
+++ frontend/src/api/client.ts
@@ -1,7 +1,9 @@
 import axios, { AxiosError } from 'axios'
 import { useAuthStore } from '../store/authStore'
 
-const BASE_URL = '/api/v1'
+const BASE_URL = import.meta.env.VITE_API_TARGET
+  ? `${import.meta.env.VITE_API_TARGET}/api/v1`
+  : '/api/v1'
 
 export const api = axios.create({
   baseURL: BASE_URL,
